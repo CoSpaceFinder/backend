94b1291b8ce3d774b3b763e6c80c1f9e066541d7
zsikorska <sikorska.zuzia@gmail.com>
2024-03-15 14:27:56 +0100

    started spring boot project with basic dependencies

---
+++ src/main/java/pwr/csfbackend/CsfBackendApplication.java
@@ -0,0 +1,13 @@
+package pwr.csfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsfBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pwr/csfbackend/CsfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.csfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
